cc85677b1708d2653bfe0bff26dacc0eeb9c398a
Duarte,Andres <duarteandresj@gmail.com>
2023-11-21 15:42:05 -0300

    Se finaliza index,se agrega la seccion de resume

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
+window.addEventListener("scroll", function () {
+  let offset = window.scrollY;
+  document.getElementById("home").style.backgroundPositionY =
+    offset * -0.7 + "px";
+  document.getElementById("about-me").style.backgroundPositionY =
+    offset * 0.5 + "px";
+  document.getElementById("contact").style.backgroundPositionY =
+    offset * 0.5 + "px";
+});
+const $form = document.getElementById("form");
+const $btnEnviar = document.getElementById("btn-Enviar");
+$btnEnviar.addEventListener("click", event=> enviarFormulario(event));
 
+function enviarFormulario(event) {
+event.preventDefault();
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let mensaje = document.getElementById("mensaje").value;
+  if (nombre == ""||email==""||mensaje == "") {
+    alert("¡Alerta!\nAlguno de los campos del formulario se encuentra vacío. Por favor, complete todos los campos antes de enviar el formulario.");
+  } else {
+    alert(
+      "Formulario enviado:\nNombre: " +
+        nombre +
+        "\nCorreo Electrónico: " +
+        email +
+        "\nMensaje: " +
+        mensaje
+    );
+    setTimeout(() => {
+        $form.reset();
+    }, 500);
+    
+     }
+}
